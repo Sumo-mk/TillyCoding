c7d3eee4508fd31fd9aaa696eae60812a6ff42d5
Sumo-mk <111373798+Sumo-mk@users.noreply.github.com>
2022-11-19 11:10:02 +0000

    1 Names finished

---
+++ 1 Names.py
@@ -0,0 +1,9 @@
+#asks for first name and surname and outputs full name
+
+# prompt for first name
+f_name = input("Please input your first name") 
+#prompt for last name
+l_name = input("Please input your last name")
+
+#prints full name 
+print(f_name , l_name , ".")
